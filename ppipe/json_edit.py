d80f6c6ac5f7b9c9ac8973eb90e2b677374aac71
samapriya <samapriya.roy@gmail.com>
2019-02-07 09:31:00 -0500

    Updated v0.4.3
    Added copy notifications

--- ppipe/json_edit.py
+++ ppipe/json_edit.py
@@ -1,3 +1,23 @@
+from __future__ import print_function
+__copyright__ = """
+
+    Copyright 2019 Samapriya Roy
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+"""
+__license__ = "Apache 2.0"
+
 data_pso={"id": "","bands": [{"id": "b1"},{"id": "b2"},{"id": "b3"},{"id": "b4"}],"missingData": {"value": "0"},"properties": {"system:time_start":"","product_type":"","orbit":"",
 "provider":"","instrument":"","satellite_id":"","tile_id":"","number_of_bands":"","epsg_code":"","resampling_kernel":"","number_of_rows":"","number_of_columns":"","gsd":"",
 "cloud_cover":"","incidence_angle":"","sun_azimuth":"","sun_elevation":"","azimuth_angle":"","spacecraft_angle":"","radiometric_scale_factor":"","reflectance_coefficient_b1":"",
